dadb1b2c43aa0341cda7fc1d5646f27ab761c532
SimJJ96 <simjingjie13@gmail.com>
2023-03-05 16:03:25 +0800

    add solution 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valueToRemove) {
+    // Use hashmap  to store the item for O(1) get
+    let hashMap = new Map ();
+    
+    // key = arr value, value = index;
+    for (let i=0; i < array.length; i++) {
+        hashMap.set(array[i], i);
+    }
 
+    valueToRemove.forEach(element => {
+        hashMap.delete(element);
+    });
+
+    return Array.from(hashMap.keys());
 };
 
 // Do not edit below this line
