6839a2dfe1720ba32081e863501d3633dc69d759
SimJJ96 <simjingjie13@gmail.com>
2023-03-05 16:40:50 +0800

    add solution 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    return years % 400 === 0 ? true : years % 4 === 0 && years % 100 !== 0 ? true : false;
 };
 
 // Do not edit below this line
