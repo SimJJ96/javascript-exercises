80c7b2ed4fa3d7b15348b6305589f83b07616e8a
SimJJ96 <simjingjie13@gmail.com>
2023-03-24 00:02:30 +0800

    add solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanString = string.replace(/\W/g, "").toLowerCase();
+    console.log(cleanString);
+    let front = 0;
+    let end = cleanString.length-1;
+    while (front < end) {
+        if (cleanString.charAt(front) !== cleanString.charAt(end)) {
+            return false;
+        }
+        front++;
+        end--;
+    }
+    return true;
 };
 
 // Do not edit below this line
