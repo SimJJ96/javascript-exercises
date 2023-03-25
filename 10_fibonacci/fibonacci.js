2d6913114d306d1d0708d3cf3afd8ffe70ebfe10
SimJJ96 <simjingjie13@gmail.com>
2023-03-25 16:16:51 +0800

    add solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    n = Number(n);
+    // Recursive Method
+    // if (n < 0) {
+    //     return 'OOPS';
+    // } else if (n === 0) {
+    //     return 0;
+    // } else if (n === 1) {
+    //     return 1;
+    // }
+    
+    // return fibonacci(n-1) + fibonacci(n-2);
+
+    // Iterative Method
+    if (n < 0) {
+        return 'OOPS';
+    }
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < n; i++) {
+        let temp = a;
+        a = b;
+        b = temp + a; 
+    }
+    
+    return b;
 };
 
 // Do not edit below this line
